eca168403b661aba76cfb2fe373a0927592c281c
unknown <svvlerdenk@gmail.com>
2023-06-07 13:45:12 +0300

    s7g2

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import Form from "./components/Form";
+
+function App() {
+  const [uyeler, setUyeler] = useState([]);
+  const [editId, setEditId] = useState(null);
+  const handleEdit = (id) => {
+    setEditId(id);
+    console.log(id);
+  };
+  return (
+    <div className="App">
+      <Form editId={editId} uyeler={uyeler} setUyeler={setUyeler} />
+      {uyeler.map((uye, index) => (
+        <div key={uye.id}>
+          <span>{uye.firstName + " " + uye.lastName}</span>
+          <span>
+            <button onClick={() => handleEdit(uye.id)}>Düzelt</button>
+          </span>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
